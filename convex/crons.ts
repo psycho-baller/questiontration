63334ffff6090dfe46173abb2d003b2eefe40646
Rami Maalouf <rami.rami@ucalgary.ca>
2025-09-25 00:47:17 -0600

    cron jobs off for now

--- convex/crons.ts
+++ convex/crons.ts
@@ -3,13 +3,13 @@ import { internal } from "./_generated/api";
 
 const cron = cronJobs();
 
-cron.interval(
-  "public game progress",
-  { seconds: 10 },
-  internal.publicGame.progress,
-  {
-    fromStage: "reveal",
-  }
-);
+// cron.interval(
+//   "public game progress",
+//   { seconds: 10 },
+//   internal.publicGame.progress,
+//   {
+//     fromStage: "reveal",
+//   }
+// );
 
 export default cron;
